1b101d91939d09465c5968e030a89847ef140c24
marcelohbb <marcelo_aut@hotmail.com>
2020-04-18 14:58:37 -0400

    subindo aplicação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+      if (balance.total < value)
+        throw new AppError('No balance to include new outcome.');
+    }
+
+    let categoryDb = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryDb) {
+      categoryDb = categoryRepository.create({ title: category });
+      await categoryRepository.save(categoryDb);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryDb.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,29 @@
+import path from 'path';
+import csvtojson from 'csvtojson';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+import uploadConfig from '../config/upload';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(transactionsFilename: string): Promise<Transaction[]> {
+    const transactionsFilePath = path.join(
+      uploadConfig.directory,
+      transactionsFilename,
+    );
+
+    const transactions = await csvtojson().fromFile(transactionsFilePath);
+
+    const createTransaction = new CreateTransactionService();
+
+    const createdTransactions = await Promise.all(
+      transactions.map(async transaction => {
+        const createdTransaction = await createTransaction.execute(transaction);
+        return createdTransaction;
+      }),
+    );
+
+    return createdTransactions;
   }
 }
 
